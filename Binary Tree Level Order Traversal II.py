0ed6cee2ae48504a3f84e4d4d281371de20a7d56
paras55 <52483274+paras55@users.noreply.github.com>
2021-04-25 16:50:02 +0530

    Create Binary Tree Level Order Traversal II.py

---
+++ Binary Tree Level Order Traversal II.py
@@ -0,0 +1,25 @@
+# https://www.youtube.com/watch?v=MBZ-gBkjdMc&ab_channel=DEEPTITALESRA
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        if root is not None:
+            queue=[root]
+            level=[]
+            next_queue=[]
+            result=[]
+            #isme humne naya queue bna liya aur ek hi queme me bar bar nahi kara
+            while len(queue)>0:
+                for root in queue:
+                    level.append(root.val)
+                    if root.left is not None:
+                        next_queue.append(root.left)
+                    if root.right is not None:
+                        next_queue.append(root.right)
+                result.append(level)
+                level=[]
+                queue=next_queue
+                next_queue=[]
+            result.reverse()
+            return result
+                
+                        
