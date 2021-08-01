c7fb25b6d2ab79625d3a9657df6b6661183dfeb5
paras55 <52483274+paras55@users.noreply.github.com>
2021-08-01 14:08:44 +0530

    Create Top View of Binary Tree1.py

---
+++ Top View of Binary Tree1.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+#GFG
+
+class Solution:
+    def topView(self,root):
+            val={}
+            h=0
+            queue=[[root,h]]
+            while len(queue)>0:
+                p=queue.pop(0)
+                if p[1] not in val:
+                    val[p[1]]=p[0].data
+                if p[0].left is not None:
+                    queue.append([p[0].left,p[1]-1])
+                if p[0].right is not None:
+                    queue.append([p[0].right,p[1]+1])
+            l=[]
+            for i in val:
+                l.append(i)
+            l.sort()
+            p=[]
+            for k in l:
+                p.append(val[k])
+            return p
+            
+                
+            
